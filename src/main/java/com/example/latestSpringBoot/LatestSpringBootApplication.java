e2a426a5a0d4e5d56093bcf0b0c99811bb7d8c59
Kai <dk786161616@gmail.com>
2025-02-21 00:06:53 +0530

    SpringBoot Application setup 2025 Feb

---
+++ src/main/java/com/example/latestSpringBoot/LatestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.latestSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatestSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/latestSpringBoot/LatestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.latestSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
